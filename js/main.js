57233b3b5243b19035445a38b5d7cbc850912506
Johanna Schreiner <JS@Johannas-MBP.BigPond>
2017-11-08 12:27:22 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 
 
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank : "queen",
+suit : "hearts",
+cardImage :	"images/queen-of-hearts.png"
+},
+{
+rank : "queen",
+suit : "diamonds",
+cardImage :	"images/queen-of-diamonds.png"	
+},
+{
+rank : "king",
+suit : "hearts",
+cardImage :	"images/king-of-hearts.png"	
+},
+{
+rank : "king",
+suit : "diamonds",
+cardImage :	"images/king-of-diamonds.png"	
+}
+];
 
 var cardsInPlay = [];
 
@@ -16,7 +37,7 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 	checkForMatch;
-	console.log("User flipped" + " " + cards[cardId]);
+	console.log("User flipped" + " " + cards[cardId].rank);
 
     	
 			
@@ -26,6 +47,9 @@ flipCard(0);
 flipCard(2);
 
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+
+console.log(cardImage);
+console.log(suit);
 
 
